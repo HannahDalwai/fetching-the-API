fd648b60bf5521530cc378a2c47288bf96466bd0
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-02-07 16:27:40 +0200

    dom

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ fetch('http://localhost:8080/products')
   .then((response) => response.json())
   .then((json) => console.log(json));
 ///
+console.log()
+
+
+for (let product of products){
+  document.getElementById('content').innerHTML += `
+    <img src="${ product.img }">
+    <p>${ item.text }</p>
+  `
+}
